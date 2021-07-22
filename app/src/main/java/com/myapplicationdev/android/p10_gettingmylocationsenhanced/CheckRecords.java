43a8adb188dbf8fc64bfab3453482ee542cb2405
li tong <lililililitongg@gmail.com>
2021-07-22 23:58:17 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/p10_gettingmylocationsenhanced/CheckRecords.java
+++ app/src/main/java/com/myapplicationdev/android/p10_gettingmylocationsenhanced/CheckRecords.java
@@ -2,8 +2,13 @@ package com.myapplicationdev.android.p10_gettingmylocationsenhanced;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -13,6 +18,7 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class CheckRecords extends AppCompatActivity {
@@ -21,7 +27,7 @@ public class CheckRecords extends AppCompatActivity {
     TextView tvNumOfRecords;
     ListView lvRecords;
     ArrayAdapter<String> aa;
-    ArrayList<String> alString;
+    ArrayList<String> alRecords;
     String folderLocation;
 
     @Override
@@ -33,7 +39,7 @@ public class CheckRecords extends AppCompatActivity {
         btnFavorite = findViewById(R.id.btnFavorites);
         tvNumOfRecords = findViewById(R.id.textViewNumOfRecords);
         lvRecords = findViewById(R.id.listViewRecords);
-        alString = new ArrayList<String>();
+        alRecords = new ArrayList<String>();
 
         folderLocation = getFilesDir().getAbsolutePath() + "/MyFolder";
         File targetFile = new File(folderLocation, "data.txt");
@@ -44,11 +50,9 @@ public class CheckRecords extends AppCompatActivity {
                 BufferedReader br = new BufferedReader(reader);
                 String line = br.readLine();
                 while (line != null){
-                    alString.add(line);
+                    alRecords.add(line);
                     numOfRecords += 1;
                     line = br.readLine();
-//                    numOfRecords += 1;
-//                    alString.add(line);
                 }
                 tvNumOfRecords.setText("Number of Records: "+ numOfRecords);
                 br.close();
@@ -57,12 +61,68 @@ public class CheckRecords extends AppCompatActivity {
                 Toast.makeText(CheckRecords.this, "Failed to read!", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
-            aa = new ArrayAdapter<String>(CheckRecords.this, android.R.layout.simple_list_item_1, alString);
+            aa = new ArrayAdapter<String>(CheckRecords.this, android.R.layout.simple_list_item_1, alRecords);
             lvRecords.setAdapter(aa);
-
-
         }
 
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alRecords.clear();
+
+                folderLocation = getFilesDir().getAbsolutePath() + "/MyFolder";
+                File targetFile = new File(folderLocation, "data.txt");
+                if (targetFile.exists() == true){
+                    int numOfRecords = 0;
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+                        String line = br.readLine();
+                        while (line != null){
+                            alRecords.add(line);
+                            numOfRecords += 1;
+                            line = br.readLine();
+                        }
+                        tvNumOfRecords.setText("Number of Records: "+ numOfRecords);
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(CheckRecords.this, "Failed to read!", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    aa = new ArrayAdapter<String>(CheckRecords.this, android.R.layout.simple_list_item_1, alRecords);
+                    lvRecords.setAdapter(aa);
+                }
+            }
+        });
+        lvRecords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String favRecords = alRecords.get(i);
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(getApplicationContext());
+                myBuilder.setMessage("Add this location in your favorite list?");
+                myBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        try {
+                            folderLocation = getFilesDir().getAbsolutePath() + "/MyFolder";
+                            File targetFile_I = new File(folderLocation, "favourites.txt");
+                            FileWriter writer_I = new FileWriter(targetFile_I, true);
+                            writer_I.write(favRecords + "\n");
+                            writer_I.flush();
+                            writer_I.close();
+                        } catch (Exception e){
+                            Toast.makeText(getApplicationContext(), "Failed to write!", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                myBuilder.setNegativeButton("No", null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/myapplicationdev/android/p10_gettingmylocationsenhanced/MyService2.java
@@ -0,0 +1,117 @@
+package com.myapplicationdev.android.p10_gettingmylocationsenhanced;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class MyService2 extends Service {
+
+    LocationRequest mLocationRequest;
+    LocationCallback mLocationCallBack;
+    FusedLocationProviderClient client;
+    String folderLocation;
+
+    public MyService2() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(3000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+        mLocationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location data = locationResult.getLastLocation();
+                    double lat = data.getLatitude();
+                    double lng = data.getLongitude();
+
+                    try {
+                        folderLocation = getFilesDir().getAbsolutePath() + "/MyFolder";
+                        File targetFile_I = new File(folderLocation, "data.txt");
+                        FileWriter writer_I = new FileWriter(targetFile_I, true);
+                        writer_I.write(lat + ", " + lng + "\n");
+                        writer_I.flush();
+                        writer_I.close();
+                    } catch (Exception e){
+                        Toast.makeText(getApplicationContext(), "Failed to write!", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "failed to receive location", Toast.LENGTH_SHORT).show();
+                }
+            };
+        };
+
+        //Folder creation for Internal Storage
+        folderLocation = getFilesDir().getAbsolutePath() + "/MyFolder";
+        File folder = new File(folderLocation);
+        if (folder.exists() == false){
+            boolean result = folder.mkdir();
+            if (result = true){
+                Log.d("File Read/Write", "Folder created");
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        client = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        if (checkPermission() == true){
+            client.requestLocationUpdates(mLocationRequest, mLocationCallBack, null);
+
+        } else {
+            ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},0);
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        client.removeLocationUpdates(mLocationCallBack);
+        Toast.makeText(getApplicationContext(), "Remove Location Update Successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkPermission(){
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED && permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
